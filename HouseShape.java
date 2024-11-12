ebcca50fe354e8f93dcdb875c4c77e3e944b829d
andriant <thanandrian@gmail.com>
2024-11-12 09:01:51 -0800

    Initial commit

---
+++ HouseShape.java
@@ -0,0 +1,25 @@
+import java.awt.*;
+import java.awt.geom.*;
+import javax.swing.*;
+public class House implements Icon{
+
+    @Override
+    public int getIconHeight()
+    {
+        return 0;
+
+    }
+
+    @Override
+    public int getIconWidth()
+    {
+        return 0;
+
+    }
+
+    @Override
+    public void paintIcon(Component c, Graphics g, int x, int y)
+    {
+
+    }
+}
